932a5591c73841c6c252504719bf28793bbfa650
ebihara99999 <dev.biibiebi@gmail.com>
2024-05-22 17:25:28 +0900

    Resolve 05-q.

--- c_lang/9cguide/hello.c
+++ c_lang/9cguide/hello.c
@@ -2,6 +2,10 @@
 
 int main(void)
 {
-	printf("40/13 = %d...%d\n", 40 / 13, 40 % 13);
+	float price;
+	int change;
+	price = (int)((198 * 1 + 138 * 2) * 1.05);
+	change = 1000 - price;
+	printf("Change is %d\n", change);
 	return 0;
 }
