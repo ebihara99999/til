b651a37f1d35037225ee426b12b0a7b550889986
ebihara99999 <dev.biibiebi@gmail.com>
2024-05-23 16:47:16 +0900

    An answer of 11-3-1.

--- c_lang/9cguide/answer.c
+++ c_lang/9cguide/answer.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 
+int is_olympic_year(int);
+
 int main()
 {
-	int score;
-	do
-	{
-		printf("Enter a score: \n");
-		scanf("%d", &score);
-	} while (!(score <= 100 && score >= 0));
+	int year;
+	printf("Enter a year: \n");
+	scanf("%d", &year);
+	int result = is_olympic_year(year);
 
-	printf("Score is %d\n", score);
-	return 0;
+	if (result == 1)
+	{
+		printf("Olympic year\n");
+	}
+	else
+	{
+		printf("Not an Olympic year\n");
+	}
+}
+
+int is_olympic_year(int year)
+{
+	int start = 2000;
+
+	if ((year - start) % 4 == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
 }
