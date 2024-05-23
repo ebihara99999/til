53d820c9d97f9fa882856cc87c3541ff76c98b7c
ebihara99999 <dev.biibiebi@gmail.com>
2024-05-23 16:10:46 +0900

    An answer of 4-1.

--- c_lang/9cguide/hello.c
+++ c_lang/9cguide/hello.c
@@ -2,9 +2,12 @@
 
 int main(void)
 {
-		int price;
-		scanf("%d", &price);
-		printf("一割引き: %d, 三割引き: %d, 五割引: %d, 八割引: %d\n",
-						(int)(price * 0.9), (int)(price * 0.7), (int)(price * 0.5), (int)(price * 0.2));
-		return 0;
+	int year;
+	scanf("%d", &year);
+	if (year % 4 == 0) {
+		printf("It is an olympic year\n");
+	} else {
+		printf("It is NOT an olympic year\n");
+	}
+
 }
