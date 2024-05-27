ef6d4f1f27ce596b6ec8889535ed2d85742a54ae
ebihara99999 <dev.biibiebi@gmail.com>
2024-05-27 20:03:33 +0900

    An answer of 15-3-1.

--- c_lang/9cguide/answer.c
+++ c_lang/9cguide/answer.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
-#include <string.h>
+
+int minmax(int array[], int *min, int *max);
 
 int main(void)
 {
-	char first_name[32];
-	char last_name[32];
+	int i = 0, max = 0, min = 100;
+	int array[10];
+	printf("Enter a number between 0 and 100:\n");
+	printf("Enter -1 to quit\n");
+	do
+	{
+		printf("Number %d:\n", i + 1);
+		scanf("%d", &array[i]);
+		i++;
+	} while (array[i - 1] != -1);
 
-	scanf("%32s %32s", first_name, last_name);
-	printf("%s\n", strcat(first_name, last_name));
+	minmax(array, &min, &max);
+	printf("Max value is %d\nMin value is %d.\n", max, min);
 	return 0;
+}
+
+int minmax(int array[], int *min, int *max)
+{
+	int i = 0;
+	while (array[i] != -1)
+	{
+		if (array[i] > *max)
+		{
+			*max = array[i];
+		}
+		if (array[i] < *min)
+		{
+			*min = array[i];
+		}
+		i++;
+	}
 }
