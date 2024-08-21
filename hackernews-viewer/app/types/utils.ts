55da52484e11ca2d2fe6834d4179888043da5abd
ebihara99999 <dev.biibiebi@gmail.com>
2024-08-21 10:22:45 +0900

    Make utility functions to fetch data.
    The source in app/utils is from https://codezine.jp/.

---
+++ hackernews-viewer/app/types/utils.ts
@@ -0,0 +1,11 @@
+interface Item {
+  id: number;
+  title: string;
+  by: string;
+  descendants: number;
+  kids: [number];
+  score: number;
+  time: number;
+  type: string;
+  url: string;
+}
---
+++ hackernews-viewer/app/utils/hackerNews.server.ts
@@ -0,0 +1,12 @@
+/** Hacker Newsの人気・最新の記事500件の記事IDを取得する */
+export async function getTopStories(): Promise<number[]> {
+  return fetch("https://hacker-news.firebaseio.com/v0/topstories.json").then(
+    (res) => res.json()
+  );
+}
+/** 記事の詳細データを取得する */
+export async function getItem(id: Item["id"]): Promise<Item> {
+  return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(
+    (res) => res.json()
+  );
+}
