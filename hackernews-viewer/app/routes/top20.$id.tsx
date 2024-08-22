a79888081064b40037f96b611aae2bce54c5cc6d
ebihara99999 <dev.biibiebi@gmail.com>
2024-08-22 12:22:59 +0900

    Show comments in the main area.

--- hackernews-viewer/app/routes/top20.$id.tsx
+++ hackernews-viewer/app/routes/top20.$id.tsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/react";
 import invariant from "tiny-invariant";
-import { getItem } from "~/utils/hackerNews.server";
+import { getItem, getComment } from "~/utils/hackerNews.server";
 import stylesUrl from "~/styles/article.css";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 
@@ -13,19 +13,35 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
   const { id } = params;
   invariant(id, 'Request with id.')
   const item = await getItem(id);
+  const kidsItems: Comment[] = await Promise.all(
+    item.kids.map((kidsItemId) => getComment(kidsItemId))
+  );
 
   return json({
     item,
+    kids: kidsItems,
   });
 };
 
 export default function Top20IdRoute() {
   const loaderData = useLoaderData<typeof loader>();
   const item = loaderData.item;
+  const kids = loaderData.kids;
 
   return (
     <article>
       <h1>{item.title}</h1>
+      <p>by {item.by} on {new Date(item.time * 1000).toLocaleString()}</p>
+      <p><a href={item.url}>{item.url}</a></p>
+
+      <h2>Comments</h2>
+      {kids.map((kidsItem) => (
+        <div key={kidsItem.id}>
+          <h3>by: {kidsItem.by}</h3>
+          <p>{kidsItem.text}</p>
+          <p>{new Date(kidsItem.time * 1000).toLocaleString()}</p>
+        </div>
+      ))}
     </article>
   )
 }
